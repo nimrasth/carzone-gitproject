6da40d9bc2755441c987ad30dee98be24f96c875
nimrasth <nimrashrestha@gmail.com>
2022-09-28 17:12:27 +0545

    team model completed

--- carzone/settings.py
+++ carzone/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'carzone.db',
+        'USER': 'postgres',
+        'PASSWORD': 'POstgreSQL',
+        'HOST': 'localhost',
+        
     }
 }
 
@@ -124,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+# media settings configuration
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Team
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/home.html', data)
 
 def about(request):
     return render(request, 'pages/about.html')
