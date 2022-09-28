e8b10979b7e8272dc25d4d9968e877d862f9b64c
nimrasth <nimrashrestha@gmail.com>
2022-09-28 12:28:55 +0545

    created pages and confogured navigation links

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def test(request):
+    return HttpResponse("testing")
+    
